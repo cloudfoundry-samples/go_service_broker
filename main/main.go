70dc8fd10b27e914ddc436dc9d7aa2f0f24375ef
xingzhou <xingzhou@cn.ibm.com>
2015-04-15 14:22:33 +0800

    Implement the catalog API of a Service Broker

--- main/main.go
+++ main/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
-	ac "github.com/xingzhou/go_service_broker/aws_client"
+	// ac "github.com/xingzhou/go_service_broker/aws_client"
 	. "github.com/xingzhou/go_service_broker/web_server"
 )
 
 func main() {
 	server := CreateServer()
 
-	client := ac.NewClient("us-east-1")
-	err := client.CreateInstance()
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
+	// client := ac.NewClient("us-east-1")
+	// err := client.CreateInstance()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	panic(err)
+	// }
 
 	server.Start()
 }
--- web_server/server.go
+++ web_server/server.go
@@ -1,10 +1,6 @@
 package web_server
 
 import (
-	m "github.com/xingzhou/go_service_broker/module"
-
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -32,13 +28,4 @@ func (s *Server) Start() {
 	http.Handle("/", router)
 
 	http.ListenAndServe(":8001", nil)
-}
-
-func (s *Server) catalog(w http.ResponseWriter, r *http.Request) {
-
-	catalog := m.Catalog{}
-
-	w.Header().Set("Content-type", "application/json")
-	data, _ := json.Marshal(catalog)
-	fmt.Fprintf(w, string(data))
 }
